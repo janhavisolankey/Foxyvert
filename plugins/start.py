1ad718acef642af259632ba957c5017699abb881
janhavisolankey <janhavisolankey@gmail.com>
2025-03-16 22:39:45 +0530

    Create start.py

---
+++ plugins/start.py
@@ -0,0 +1,55 @@
+# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
+# Ask Doubt on telegram @CodeflixSupport
+
+import asyncio
+import os
+import random
+import sys
+import time
+import string
+import string as rohit
+from pyrogram import Client, filters, __version__
+from pyrogram.enums import ParseMode
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
+from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated, UserNotParticipant
+from bot import Bot
+from config import *
+from helper_func import *
+from database.database import *
+
+# File auto-delete time in seconds 43200
+FILE_AUTO_DELETE = 43200  # Example: 43200 seconds (1 hour)
+TUT_VID = f"{TUT_VID}"
+
+@Bot.on_message(filters.command('start') & filters.private & subscribed1 & subscribed2 & subscribed3 & subscribed4)
+async def start_command(client: Client, message: Message):
+    id = message.from_user.id
+    if not await present_user(id):
+        try:
+            await add_user(id)
+        except:
+            pass
+
+    # Check if user is an admin and treat them as verified
+    if id in ADMINS:
+        verify_status = {
+            'is_verified': True,
+            'verify_token': None,  # Admins don't need a token
+            'verified_time': time.time(),
+            'link': ""
+        }
+    else:
+        verify_status = await get_verify_status(id)
+
+        # If TOKEN is enabled, handle verification logic
+        if TOKEN:
+            if verify_status['is_verified'] and VERIFY_EXPIRE < (time.time() - verify_status['verified_time']):
+                await update_verify_status(id, is_verified=False)
+
+            if "verify_" in message.text:
+                _, token = message.text.split("_", 1)
+                if verify_status['verify_token'] != token:
+                    return await message.reply("Your token is invalid or expired. Try again by clicking /start.")
+                await update_verify_status(id, is_verified=True, verified_time=time.time())
+                if verify_status["link"] == "":
